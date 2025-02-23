63cfc9ec4826e451a618421a1cacb54c29791eae
chenxin.ma <chenxin.ma@qq.com>
2025-02-23 10:18:00 +0800

    添加测试用例说明

--- tests/test_age_cypher_agent.py
+++ tests/test_age_cypher_agent.py
@@ -1,18 +1,26 @@
+"""
+测试 age_cypher_agent.py 生成Cypher查询
+"""
 import asyncio
+import logfire
 from bot.agent import Deps, GRAPH_NAME, DSN, CypherQuery
 from bot.agent.age_cypher_agent import age_agent
-
-import logfire
 
 # 配置日志
 logfire.configure(environment='local')
 
 async def bot_call(question: str):
+    """
+    age_cypher_agent.py 生成Cypher查询
+    """
     deps = Deps(g_name=GRAPH_NAME, url=DSN)
     result = await age_agent.run(question, deps=deps)
     return result.data
 
 def test_01():
+    """
+    测试 age_cypher_agent.py 生成Cypher查询 case1
+    """
     question = "列出所有应用"
     data = asyncio.run(bot_call(question))
     print(data)
