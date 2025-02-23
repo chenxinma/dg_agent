f945757d4bf0caa707605a100cfb245df4483d73
chenxin.ma <chenxin.ma@qq.com>
2025-02-23 11:48:20 +0800

    更新元模型定义

--- graph/reload_graph.py
+++ graph/reload_graph.py
@@ -1,46 +1,52 @@
+"""
+元模型导入图数据库
+"""
+from io import StringIO
+
 import click
 import age
 from lxml import etree
-from io import StringIO
+from tqdm import tqdm
 
 from .domain import Domain
 from .entity import Entity
 from .application import Application
 from . import GRAPH_NAME, DSN
-from tqdm import tqdm
 
 class ConceptSturture:
+    """构建元模型 业务域、应用、数据实体"""
     def __init__(self, xml_root):
         self._domain_list = Domain.load(xml_root)
         self._app_list = Application.load(xml_root)
         self._entity_list = Entity.load(xml_root)
         self._links = []
-                
+
         for _app in self._app_list:
             for _domain in self._domain_list:
                 if _domain.contains(_app):
                     _domain.apps.append(_app)
                     _app.domain = _domain
-                    
+
         for _entity in self._entity_list:
             for _app in self._app_list:
                 if _app.contains(_entity):
                     _app.entities.append(_entity)
                     _entity.app = _app
-                    
+
         self._dup = {}
         for _e1 in self._entity_list:
             for _e2 in self._entity_list:
                 if _e1.name == _e2.name and _e1.app != _e2.app:
                     ids = sorted([_e1.id, _e2.id])
                     _key = str(ids)
-                    if not _key in self._dup:
+                    if  _key not in self._dup:
                         self._dup[_key] = (_e1, _e2)
 
     def link_entities(self, xml_root):
+        """数据实体间的关联关系"""
         _gids = [ e.gid for e in self._entity_list ]
         _entities = dict(zip(_gids, self._entity_list))
-        
+
         skips = ["x4Fgb3jFKm9Y57xnfF53-16"]
         for _node in xml_root.xpath('//mxCell[contains(@style, "endArrow")]'):
             style = _node.attrib['style']
@@ -54,81 +60,82 @@ class ConceptSturture:
                     if _source in skips or _target in skips:
                         continue
                     self._links.append((_entities[_source], _entities[_target]))
-                except:
-                    print(_node.attrib['id'], _source, _target)
+                except (KeyError, AttributeError) as ex:
+                    print(_node.attrib['id'], _source, _target, ex)
 
     def make_graph(self, ag):
-        print("    Saving domain_list")
+        """写入图数据库"""
+        print("    Saving BusinessDomain list")
         conn_1 = ag.connection
         with conn_1.cursor() as _cursor:
             for _domain in self._domain_list:
-                _cursor.execute('''SELECT * from cypher(%s, $$ 
-                CREATE (n:Domain {name: %s, nid: %s, code: %s}) 
+                _cursor.execute('''SELECT * from cypher(%s, $$
+                  CREATE (n:BusinessDomain {name: %s, nid: %s, code: %s}) 
                 $$) as (v agtype); ''', (GRAPH_NAME, _domain.name, _domain.id, _domain.code))
 
         print("    Saving app_list")
         with conn_1.cursor() as _cursor:
             for _app in self._app_list:
-                _cursor.execute('''SELECT * from cypher(%s, $$ 
-                CREATE (n:Application {name: %s, nid: %s}) 
+                _cursor.execute('''SELECT * from cypher(%s, $$
+                  CREATE (n:Application {name: %s, nid: %s}) 
                 $$) as (v agtype); ''', (GRAPH_NAME, _app.name, _app.id))
 
         print("    Saving domain-app belong")
         with conn_1.cursor() as _cursor:
             for _app in self._app_list:
-                _cursor.execute('''SELECT * from cypher(%s, $$ 
-                MATCH (a:Application), (b:Domain)
-                WHERE a.nid = %s AND b.nid = %s
-                CREATE (b)-[e:BELONG {name: %s}]->(a)
-                RETURN e
-                $$) as (e agtype); ''', 
-                (GRAPH_NAME, _app.id, _app.domain.id, 'contain'))
+                _cursor.execute('''SELECT * from cypher(%s, $$
+                  MATCH (a:Application), (d:BusinessDomain)
+                  WHERE a.nid = %s AND d.nid = %s
+                  CREATE (d)-[e:CONTAINS]->(a)
+                  RETURN e
+                $$) as (e agtype); ''',
+                (GRAPH_NAME, _app.id, _app.domain.id))
         ag.commit()
-        
+
         print("    Saving entity_list and entity-app REL")
         conn_1 = ag.connection
         with conn_1.cursor() as _cursor:
             for _entity in self._entity_list:
-                _cursor.execute('''SELECT * from cypher(%s, $$ 
-                CREATE (n:Entity {name: %s, nid: %s}) 
+                _cursor.execute('''SELECT * from cypher(%s, $$
+                  CREATE (n:DataEntity {name: %s, nid: %s}) 
                 $$) as (v agtype); ''', (GRAPH_NAME, _entity.name, _entity.id))
 
-                _cursor.execute('''SELECT * from cypher(%s, $$ 
-                MATCH (a:Application), (b:Entity)
-                WHERE a.nid = %s AND b.nid = %s
-                CREATE (a)-[e:REL]->(b)
-                RETURN e
-                $$) as (e agtype);''', (GRAPH_NAME, _entity.app.id, _entity.id))
-                
-        print("    Saving entity duplicated")
-        with conn_1.cursor() as _cursor:
-            for _key in self._dup:
-                _e1, _e2 = self._dup[_key]
                 _cursor.execute('''SELECT * from cypher(%s, $$
-                    MATCH (a:Entity), (b:Entity)
+                  MATCH (a:Application), (b:DataEntity)
+                  WHERE a.nid = %s AND b.nid = %s
+                  CREATE (a)-[e:USES]->(b)
+                  RETURN e
+                $$) as (e agtype);''', (GRAPH_NAME, _entity.app.id, _entity.id))
+
+        print("    Saving entity FLOWS_TO")
+        with conn_1.cursor() as _cursor:
+            for _e1, _e2 in self._dup.values():
+                _cursor.execute('''SELECT * from cypher(%s, $$
+                    MATCH (a:DataEntity), (b:DataEntity)
                     WHERE a.nid = %s AND b.nid = %s
-                    CREATE (a)-[e:DUPLICATE]->(b)
+                    CREATE (a)-[e:FLOWS_TO]->(b)
                     RETURN e
                 $$) as (e agtype);''', (GRAPH_NAME, _e1.id, _e2.id))
-        
-        print("    Saving entity-entity link")
+
+        print("    Saving entity-entity RELATED_TO")
         with conn_1.cursor() as _cursor:
             for _e1, _e2 in tqdm(self._links):
                 _cursor.execute('''SELECT * from cypher(%s, $$
-                    MATCH (a:Entity), (b:Entity)
+                    MATCH (a:DataEntity), (b:DataEntity)
                     WHERE a.nid = %s AND b.nid = %s
-                    CREATE (a)-[e:LINK]->(b)
+                    CREATE (a)-[e:RELATED_TO]->(b)
                     RETURN e
                 $$) as (e agtype);''', (GRAPH_NAME, _e1.id, _e2.id))
-        
+
         ag.commit()
 
 
 def clear_graph(ag):
+    """清除图数据库"""
     conn = ag.connection
     with conn.cursor() as _cursor:
         _cursor.execute('''SELECT * from cypher(%s, $$
-            MATCH (v:Domain)
+            MATCH (v:BusinessDomain)
             DETACH DELETE v
         $$) as (v agtype); ''', (GRAPH_NAME,))
 
@@ -137,7 +144,7 @@ def clear_graph(ag):
             DETACH DELETE v
         $$) as (v agtype); ''', (GRAPH_NAME,))
         _cursor.execute('''SELECT * from cypher(%s, $$
-            MATCH (v:Entity)                
+            MATCH (v:DataEntity)                
             DETACH DELETE v                   
         $$) as (v agtype); ''', (GRAPH_NAME,))
     ag.commit()
@@ -145,12 +152,14 @@ def clear_graph(ag):
 
 @click.command()
 @click.argument('fname', type=click.Path(exists=True))
-def main(fname:str):
+def main(fname:str=None):
+    """
+    从XML文件中读取数据结构，并写入图数据库
+    """
     print(f"Reading file:{fname}")
     # 读取XML文件
-    with open(fname, 'rb') as file:  # 使用'rb'模式读取文件
-        tree = etree.parse(file)
-    
+    with open(fname, 'rb') as f:  # 使用'rb'模式读取文件
+        tree = etree.parse(f)
 
     diagram = tree.xpath('//diagram[@name="数据架构Lv1"]')[0]
 
@@ -161,7 +170,7 @@ def main(fname:str):
     cs = ConceptSturture(root)
     print("Linking relationships.")
     cs.link_entities(root)
-    
+
     print(f"Save to -> {GRAPH_NAME}")
     ag = age.connect(graph=GRAPH_NAME, dsn=DSN)
     clear_graph(ag)
--- graph/reload_table.py
+++ graph/reload_table.py
@@ -1,67 +1,80 @@
+"""物理模型导入图数据库
+"""
 import os
+import sys
 import click
 import age
-import pandas as pd
-from tqdm import tqdm
+try:
+    import pandas as pd
+except ImportError:
+    print("Pandas is not installed. Please install it using 'pip install pandas'.")
+    sys.exit(1)
 
+from tqdm import tqdm
 from . import GRAPH_NAME, DSN, generate_unique_id
 
 class MetadataSturture:
+    """构建元模型 物理表、列"""
+
     def __init__(self, metadatas:dict, df_columns:pd.DataFrame):
         self._db_set = metadatas
         self._df_columns = df_columns
-        
+
     def save_columns(self, cursor, full_table_name):
+        """保持列"""
         _df = self._df_columns[self._df_columns['Table'] == full_table_name]
         for row in _df.itertuples():
             nid = generate_unique_id(full_table_name +"."+ row.Column)
-            cursor.execute('''SELECT * from cypher(%s, $$ 
+            cursor.execute('''SELECT * from cypher(%s, $$
                     CREATE (n:Column {name: %s, dtype: %s, nid: %s}) 
-                    $$) as (v agtype); ''', (GRAPH_NAME, 
-                                            row.Column, 
+                    $$) as (v agtype); ''', (GRAPH_NAME,
+                                            row.Column,
                                             row.Type,
                                             nid))
-            cursor.execute('''SELECT * from cypher(%s, $$ 
-                    MATCH (a:Table), (b:Column)
+            cursor.execute('''SELECT * from cypher(%s, $$
+                    MATCH (a:PhysicalTable), (b:Column)
                     WHERE a.full_table_name = %s AND b.nid = %s
-                    CREATE (b)-[e:BELONG]->(a)
+                    CREATE (a)-[e:HAS_COLUMN]->(b)
                     RETURN e
-                    $$) as (e agtype); ''', 
+                    $$) as (e agtype); ''',
                     (GRAPH_NAME, full_table_name, nid))
-        
+
     def save_tables(self, ag, db:pd.DataFrame):
+        """保持物理表"""
         conn_1 = ag.connection
         with conn_1.cursor() as _cursor:
             for row in tqdm(db.itertuples(), total=db.shape[0]):
-                _cursor.execute('''SELECT * from cypher(%s, $$ 
-                    CREATE (n:Table {name: %s, table_name: %s, schema: %s, full_table_name: %s}) 
-                    $$) as (v agtype); ''', (GRAPH_NAME, 
+                _cursor.execute('''SELECT * from cypher(%s, $$
+                    CREATE (n:PhysicalTable {name: %s, table_name: %s, schema: %s, full_table_name: %s}) 
+                    $$) as (v agtype); ''', (GRAPH_NAME,
                                             row.entity,
-                                            row.table_name, 
-                                            row.schema, 
+                                            row.table_name,
+                                            row.schema,
                                             row.full_table_name))
 
-                _cursor.execute('''SELECT * from cypher(%s, $$ 
-                    MATCH (a:Entity), (b:Table)
+                _cursor.execute('''SELECT * from cypher(%s, $$
+                    MATCH (a:DataEntity), (b:PhysicalTable)
                     WHERE a.nid = %s AND b.full_table_name = %s
-                    CREATE (a)-[e:DEFINE]->(b)
+                    CREATE (a)-[e:IMPLEMENTS]->(b)
                     RETURN e
-                    $$) as (e agtype); ''', 
+                    $$) as (e agtype); ''',
                     (GRAPH_NAME, row.nid, row.full_table_name))
-                
+
                 self.save_columns(_cursor, row.full_table_name)
 
         ag.commit()
 
     def make_graph(self, ag):
+        """构建物理模型"""
         for db in self._db_set.values():
             self.save_tables(ag, db)
-        
+
 def clear_graph(ag):
+    """清除图数据库"""
     conn = ag.connection
     with conn.cursor() as _cursor:
         _cursor.execute('''SELECT * from cypher(%s, $$
-            MATCH (v:Table)
+            MATCH (v:PhysicalTable)
             DETACH DELETE v
         $$) as (v agtype); ''', (GRAPH_NAME,))
         _cursor.execute('''SELECT * from cypher(%s, $$
@@ -72,10 +85,14 @@ def clear_graph(ag):
 
 @click.command()
 @click.argument('fname', type=click.Path(exists=True))
-def main(fname:str):
+def main(fname:str=None):
+    """
+    从 fname 文件中读取元数据，并构建物理模型
+    """
+    print("Starting...")
     print(f"Reading file:{fname}")
     metadatas = pd.read_excel(fname, sheet_name=None)
-    
+
     # 获取 fname 所在的目录
     fname_dir = os.path.dirname(fname)
     # 构建 db 目录的路径
@@ -84,20 +101,20 @@ def main(fname:str):
     if not os.path.exists(db_dir):
         print(f"db 目录不存在: {db_dir}")
         return
-    
+
     # 列出 db 目录下所有 .xlsx 文件
     xlsx_files = [f for f in os.listdir(db_dir) if f.endswith('.xlsx')]
     df_all_columns = pd.DataFrame()
     for f in xlsx_files:
         df = pd.read_excel(os.path.join(db_dir, f), sheet_name="Columns")
         df_all_columns = pd.concat([df_all_columns, df])
-    
+
     print(f"Save to -> {GRAPH_NAME}")
     ag = age.connect(graph=GRAPH_NAME, dsn=DSN)
     clear_graph(ag)
-    
+
     ms = MetadataSturture(metadatas, df_all_columns)
     ms.make_graph(ag)
-    
+
 if __name__ == '__main__':
     main()
